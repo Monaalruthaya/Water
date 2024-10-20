bea427f2aaf9415efed8c6a5605d705d83ea203a
Monaalruthaya <monaalruthaya@outlook.com>
2024-10-20 14:23:30 +0300

    Initial Commit

---
+++ Water/WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterApp.swift
+//  Water
+//
+//  Created by mona alruthaya on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WaterTests/WaterTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTests.swift
+//  WaterTests
+//
+//  Created by mona alruthaya on 17/04/1446 AH.
+//
+
+import Testing
+@testable import Water
+
+struct WaterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
